eb0cf6b0dc7042e4a8c9147e1f6ca9f9bcf3065f
MrNote11 <aliyahsulaiman3@gmail.com>
2025-07-17 15:08:38 +0100

    In The WebHook

--- Api/views.py
+++ Api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponse
 from rest_framework.views import APIView
-from settings.conf import settings
+from django.conf import settings
 from rest_framework import status
 from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
