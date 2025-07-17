f6609ea4b31fa30b2107d2951efdfdd06eec15ec
MrNote11 <aliyahsulaiman3@gmail.com>
2025-07-17 14:57:56 +0100

    In The WebHook

--- Api/views.py
+++ Api/views.py
@@ -29,11 +29,17 @@ class WebhookView(APIView):
         # Handle incoming messages here
         data1 = request.body
         data2 = request.data
-        
-        if "object" in data1 and "entry" in data1:
-            if data1["object"] == abeg work
         data_load = json.loads(data1)
         data_load1 = json.load(data2) 
-        print(request.data)
+        print(f"data_load: {data_load}")
+        print(f"data_load1: {data_load1}")
+        data_load1 = json.load(data2) 
+        # if "object" in data1 and "entry" in data1:
+        #     if data1["object"] == "abeg work":
+        #         try:
+        #             for entry in data1["entry"]:
+        #                 phonenumber = entry['changes'][0]['value']['metadata']['display_phone_number']
+        #                 phonenumber = "2349126709734"
+        
         return Response({'Message received': data_load1,
                          'Raw data': data_load}, status=status.HTTP_200_OK)
